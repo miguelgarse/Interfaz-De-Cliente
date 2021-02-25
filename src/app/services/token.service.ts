f18f793c852db80ed2fd02b1901f52384c84e9dd
Miguel Garcia <miguelgarse@gmail.com>
2021-02-25 19:13:42 +0100

    Servicio para la recuperar y controlar el token

---
+++ src/app/services/token.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from "@angular/core";
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUserName';
+const AUTHORTIES_KEY = 'AuthAuthorities';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TokenService {
+
+    roles: Array<string> = [];
+
+    constructor() { }
+
+    public setToken(token: string): void {
+        window.sessionStorage.removeItem(TOKEN_KEY);
+        window.sessionStorage.setItem(TOKEN_KEY, token);
+    }
+
+    public getToken(): string {
+        return window.sessionStorage.getItem(TOKEN_KEY);
+    }
+
+    public setUserName(userName: string): void {
+        window.sessionStorage.removeItem(USERNAME_KEY);
+        window.sessionStorage.setItem(USERNAME_KEY, userName);
+    }
+
+    public getUserName(): string {
+        return window.sessionStorage.getItem(USERNAME_KEY);
+    }
+
+    public setAuthorities(authorities: string[]): void {
+        window.sessionStorage.removeItem(AUTHORTIES_KEY);
+        window.sessionStorage.setItem(AUTHORTIES_KEY, JSON.stringify(authorities));
+    }
+
+    public getAuthorities(): string[] {
+        this.roles = [];
+        if (sessionStorage.getItem(AUTHORTIES_KEY)) {
+            JSON.parse(sessionStorage.getItem(AUTHORTIES_KEY)).forEach(authority => {
+                this.roles.push(authority.authority);
+            });
+        }
+        return this.roles;
+    }
+
+    public logOut(): void {
+        window.sessionStorage.clear();
+    }
+
+    public isAdmin(): boolean {
+        let isAdmin: boolean = false;
+
+        this.getAuthorities().forEach(role => {
+            if (role == 'ROLE_ADMIN') {
+                isAdmin = true;
+            }
+        });
+
+        return isAdmin;
+    }
+}
