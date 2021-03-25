da2e5068391005e839f97683d24762b82a92c23e
Miguel Garcia <miguelgarse@gmail.com>
2021-03-25 16:46:03 +0100

    Subida de ficheros

--- src/app/components/add-data/add-data.component.ts
+++ src/app/components/add-data/add-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddDataService } from 'src/app/services/add-data.service';
 
 @Component({
   selector: 'app-add-data',
@@ -8,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class AddDataComponent implements OnInit {
 
   public qrResultString: string;
+  public fileList: File[] = [];
 
-  constructor() { }
+  constructor(private addDataService: AddDataService) { }
 
   ngOnInit() {
   }
@@ -18,4 +20,22 @@ export class AddDataComponent implements OnInit {
     this.qrResultString = resultString;
 
   } 
+
+  fileChange(event: any) {
+    if(event.target.files != null && event.target.files.length > 0){
+      let fileSize: number = event.target.files[0].size;
+
+      this.fileList.push(event.target.files[0]);
+
+      event.target.value = '';  //Vaciamos el array en el que nos vienen los ficheros que hemos seleccionado con el input
+    }
+  }
+
+  uploadFiles(){
+    this.addDataService.uploadFiles(this.fileList[0]).subscribe(response => {
+
+    }, error => {
+      console.error('Se ha producido un error. ' + error);
+    });
+  }
 }
---
+++ src/app/services/add-data.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/Usuario';
+import { constantes } from '../utils/constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddDataService {
+
+  private endPoint: string;
+  private apiEndpoint: string = "http://localhost:8090";     // LOCAL
+  // private apiEndpoint: string = "http://88.18.184.82:8090";  // INT
+  // private apiEndpoint: string = "http://138.4.92.46:8090";   // PRO
+ 
+  constructor(private http: HttpClient) { }
+
+  public uploadFiles(file: File): Observable<any> {
+    let formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    let headers = new HttpHeaders();
+    /** In Angular 5, including the header Content-Type can invalidate your request */
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+
+    return this.http.post<any>(this.apiEndpoint + "/api/client/add-data", formData,
+      { headers: headers }
+    );
+  }
+
+}
