dc896328279907089cc66a6921c511dc60505c61
Miguel Garcia <miguelgarse@gmail.com>
2021-04-14 19:32:13 +0200

    Se eliminan componentes no usados: graficas, table-clients...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,6 @@ import { ChartsModule } from 'ng2-charts';
 import { UploadService } from './services/upload.service';
 import { NavbarComponent } from './components/home/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
-import { GraficaTemperaturaComponent } from './components/graficas/grafica-temperatura/grafica-temperatura.component';
-import { GraficaHumedadComponent } from './components/graficas/grafica-humedad/grafica-humedad.component';
-import { GraficaCalidadComponent } from './components/graficas/grafica-calidad/grafica-calidad.component';
-import { GraficaLuminosidadComponent } from './components/graficas/grafica-luminosidad/grafica-luminosidad.component';
 import { TooltipModule } from 'ng2-tooltip-directive';
 import { SitemapComponent } from './components/sitemap/sitemap.component';
 import { LoginComponent } from './components/login/login.component';
@@ -19,13 +15,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routes';
 import { RegisterComponent } from './components/home/administration/register/register.component';
 import { ToastrModule } from 'ngx-toastr';
-import { TableClientsComponent } from './components/home/table-clients/table-clients.component';
 import { interceptorProvider } from './interceptors/prod-interceptor.service';
 import { AddDataComponent } from './components/home/add-data/add-data.component';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { PrincipalComponent } from './components/home/principal/principal.component';
 import { ProjectsComponent } from './components/home/projects/projects.component';
 import { MyProjectsComponent } from './components/home/my-projects/my-projects.component';
+import { NewProjectDialogComponent } from './components/home/projects/new-project-dialog/new-project-dialog.component';
 
 @NgModule({
   declarations: [
@@ -33,18 +29,14 @@ import { MyProjectsComponent } from './components/home/my-projects/my-projects.c
     HomeComponent,
     NavbarComponent,
     AboutComponent,
-    GraficaTemperaturaComponent,
-    GraficaCalidadComponent,
-    GraficaHumedadComponent,
-    GraficaLuminosidadComponent,
     SitemapComponent,
     LoginComponent,
     RegisterComponent,
-    TableClientsComponent,
     AddDataComponent,
     PrincipalComponent,
     ProjectsComponent,
-    MyProjectsComponent
+    MyProjectsComponent,
+    NewProjectDialogComponent
   ],
   imports: [
     BrowserModule, 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { GraficaTemperaturaComponent } from './components/graficas/grafica-temperatura/grafica-temperatura.component';
-import { GraficaHumedadComponent } from './components/graficas/grafica-humedad/grafica-humedad.component';
-import { GraficaCalidadComponent } from './components/graficas/grafica-calidad/grafica-calidad.component';
-import { GraficaLuminosidadComponent } from './components/graficas/grafica-luminosidad/grafica-luminosidad.component';
 import { SitemapComponent } from './components/sitemap/sitemap.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './components/home/administration/register/register.component';
-import { TableClientsComponent } from './components/home/table-clients/table-clients.component';
 import { ProdGuardService as guard } from './guards/prod-guard.service';
-import { AddDataComponent } from './components/home/add-data/add-data.component';
-import { PrincipalComponent } from './components/home/principal/principal.component';
 import { ProjectsComponent } from './components/home/projects/projects.component';
 import { MyProjectsComponent } from './components/home/my-projects/my-projects.component';
 
@@ -39,10 +32,6 @@ const routes: Routes = [
       data: { expectedRol: ['admin'] }
     }
   ]},
-  { path: 'graficaLuminosidad/:id', component: GraficaLuminosidadComponent },
-  { path: 'graficaTemperatura/:id', component: GraficaTemperaturaComponent },
-  { path: 'graficaHumedad/:id', component: GraficaHumedadComponent },
-  { path: 'graficaCalidad/:id', component: GraficaCalidadComponent },
   { path: 'about', component: AboutComponent },
   { path: 'sitemap', component: SitemapComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
